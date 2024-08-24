90747164e1f1b27ed6117785d57b1922b05ed35b
mrutyunjaya-py <mrutyunjaya655@gmail.com>
2024-08-24 23:19:51 +0530

    first commit

---
+++ 2.3-NPM/index.js
@@ -0,0 +1,9 @@
+// var generateName = require("sillyname");
+import generateName from "sillyname";
+var sillyName = generateName();
+
+// console.log(`my name is ${sillyName}.`)
+
+import superheroes from 'superheroes'
+var name = superheroes();
+console.log(`i am ${name}`)
